f6fdc74121bf4252fa286bb18a967ed13695e029
Heyjuke58 <H_Hinrichs@gmx.de>
2023-10-02 10:08:03 +0200

    added main script file

--- cli_boilerplate.py
+++ cli_boilerplate.py
@@ -13,10 +13,13 @@ def parse_args():
 
 
 def main(dir: str):
-    target_path = Path(dir + "/parse_arguments2.py")
-    assert not target_path.is_file(), "There already exists a argument_parser.py in the target directory. Remove it first."
+    target_path_parser = Path(dir + "/argument_parser.py")
+    assert not target_path_parser.is_file(), "There already exists a argument_parser.py in the target directory. Remove it first."
+    shutil.copy(Path("argument_parser.py"), target_path_parser)
 
-    shutil.copy(Path("argument_parser.py"), target_path)
+    target_path_main = Path(dir + "/main.py")
+    assert not target_path_main.is_file(), "There already exists a main.py in the target directory. Remove it first."
+    shutil.copy(Path("main.py"), target_path_main)
 
 
 if __name__ == "__main__":
