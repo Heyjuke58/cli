c5e3c9714ba8311f430a453958b4f9b3b240f5e4
Heyjuke58 <H_Hinrichs@gmx.de>
2023-10-02 10:22:54 +0200

    fixed error with files not being found by script

--- cli_boilerplate.py
+++ cli_boilerplate.py
@@ -6,20 +6,21 @@ from pathlib import Path
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", dest="dir", type=str, help="Directory where to paste the boilerplate code.")
+    parser.add_argument("-h", dest="home_dir", type=str, help="Home directory where to paste the boilerplate code.")
 
     args = parser.parse_args()
 
     return args
 
 
-def main(dir: str):
+def main(dir: str, home_dir: str):
     target_path_parser = Path(dir + "/argument_parser.py")
     assert not target_path_parser.is_file(), "There already exists a argument_parser.py in the target directory. Remove it first."
-    shutil.copy(Path("argument_parser.py"), target_path_parser)
+    shutil.copy(Path(home_dir + "/bin/argument_parser.py"), target_path_parser)
 
     target_path_main = Path(dir + "/main.py")
     assert not target_path_main.is_file(), "There already exists a main.py in the target directory. Remove it first."
-    shutil.copy(Path("main.py"), target_path_main)
+    shutil.copy(Path(home_dir + "/bin/main.py"), target_path_main)
 
 
 if __name__ == "__main__":
